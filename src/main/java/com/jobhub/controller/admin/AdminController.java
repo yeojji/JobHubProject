eec4e4a5c25066c350851bc4901b900246394973
jeeunjo <jje6658@gmail.com>
2024-02-01 17:17:23 +0900

    관리자 로그인 페이지 추가

--- src/main/java/com/jobhub/controller/admin/AdminController.java
+++ src/main/java/com/jobhub/controller/admin/AdminController.java
@@ -2,6 +2,7 @@ package com.jobhub.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.jobhub.service.admin.AdminService;
 
@@ -11,7 +12,11 @@ public class AdminController {
 	@Autowired
 	AdminService adminService;
 	
-	
+	@GetMapping("/admin")
+	public String admin() {
+		
+		return "admin/login";
+	}
 	
 	
 	
