d9530fabd87e00775933a934c26b8002854714ff
jeeunjo <jje6658@gmail.com>
2024-02-13 14:05:49 +0900

    관리자 조회 페이지 추가

--- src/main/java/com/jobhub/dto/admin/AdminSearchCondition.java
+++ src/main/java/com/jobhub/dto/admin/AdminSearchCondition.java
@@ -5,15 +5,16 @@ import lombok.Data;
 @Data
 public class AdminSearchCondition {
 
-	String id;				//Admin
-	String pw;				//Admin
+	String adminId;				//Admin
+	String adminPw;				//Admin
 	String employeeId;		//Employee
 	String name;			//Employee
-	String jobName;			//Jobs
+	String jobsName;			//Jobs
 	String email;			//Employee
 	String tel;				//Employee
 	String hireDate;		//Employee
 	String permissionCode;	//Admin
 	String creationCode;	//Admin
-
+	String userStatus;
+	
 }
